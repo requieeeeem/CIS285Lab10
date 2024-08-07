ce0dd2e305fa39eb09e82f7eed9446e2349870b4
Michael Luong <michalu@umich.edu>
2024-08-07 16:55:55 -0400

    MixedDup commit. test working

--- src/testSelectionSort.java
+++ src/testSelectionSort.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 		public void test() {
 			//testPositive();
 			//testNegative();
-			//testMixed();
+			testMixed();
 			testDuplicates();
 		}
 		public testSelectionSort() {
